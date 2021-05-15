8d8d50e10c6940705bb4b147597e072ad1cd81f1
babs20 <thevillemc@gmail.com>
2021-05-15 01:39:34 -0400

    edit font order

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,24 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      sans: [
+        'Roboto',
+        'ui-sans-serif',
+        'system-ui',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        'Segoe UI',
+        'Helvetica Neue',
+        'Arial',
+        'Noto Sans',
+        'sans-serif',
+        'Apple Color Emoji',
+        'Segoe UI Emoji',
+        'Segoe UI Symbol',
+        'Noto Color Emoji',
+      ],
+    },
     extend: {},
   },
   variants: {
