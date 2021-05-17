8bc058182daf63aee4d4bcbdee107cecf7d83b9b
babs20 <thevillemc@gmail.com>
2021-05-17 13:05:12 -0400

    add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
 import Home from './views/Home';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 function App() {
-  return <Home></Home>;
+  return (
+    <Router>
+      <Route path='/'>
+        <Home></Home>
+      </Route>
+      <Route path='/projects'>
+        <Home></Home>
+      </Route>
+      <Route path='/goals'>
+        <Home></Home>
+      </Route>
+      <Route path='/contact'>
+        <Home></Home>
+      </Route>
+    </Router>
+  );
 }
 
 export default App;
