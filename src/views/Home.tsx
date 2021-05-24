377492b077dfd676aa7ac9a7a14c4b3454075859
babs20 <thevillemc@gmail.com>
2021-05-24 18:40:06 -0400

    change routing to be able to push and make new branch

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,9 +1,11 @@
 import OpeningArticle from '../components/OpeningArticle';
+import Projects from './Projects';
 
 export const Home = (): JSX.Element => {
   return (
     <section className=''>
       <OpeningArticle />
+      <Projects />
     </section>
   );
 };
